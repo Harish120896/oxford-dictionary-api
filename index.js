fb13567575c969729194df65ccd2e228955f2a2e
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-05-29 12:52:11 +0200

    Improved Formatting

--- index.js
+++ index.js
@@ -9,40 +9,46 @@ var OxfordDictionary = function(app_id,app_key) {
 };    
  
   
-    OxfordDictionary.prototype.find = function(word,cb){
-        var options = {
-            host :  'od-api.oxforddictionaries.com',
-            port : 443,
-            path : '/api/v1/entries/en/'+word,
-            method : 'GET',
-            headers : {
-                    "Accept": "application/json",
-                    "app_id": this.config.app_id,
-                    "app_key": this.config.app_key
+OxfordDictionary.prototype.find = function(word,cb){
+    var options = {
+        host :  'od-api.oxforddictionaries.com',
+        port : 443,
+        path : '/api/v1/entries/en/'+word,
+        method : 'GET',
+        headers : {
+                "Accept": "application/json",
+                "app_id": this.config.app_id,
+                "app_key": this.config.app_key
                    }
             };
         
         
-        https.get(options, function(res) {
+    https.get(options, function(res) {
         //console.log("\nstatus code: ", res.statusCode);
         if(res.statusCode == 404){
             cb("No such entry found");
-        }else{
+        }
+
+        else {
+        
         var data = "";
+
         res.on('data', function(chunk) {
              data +=  chunk;
-        })
-        .on('end',function(){
+        }).on('end',function(){
             try {
             result = JSON.parse(data);
           } catch (exp) {
             result = {'status_code': 500, 'status_text': 'JSON Parse Failed'};
           }
             cb(null,result);
-        })
-        .on('error',function(err){
+        }).on('error',function(err){
              cb(err);    
-        });}
+        });
+
+        }
+        
         });
     }; 
+    
 module.exports = OxfordDictionary;
